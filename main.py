e2dc0c1c9d02e7784db98b935529e32b89e96d02
KevinED11 <mendez_kael@hotmail.com>
2023-10-28 18:55:37 -0700

    add generic stack implementation with python 3.12

--- main.py
+++ main.py
@@ -26,6 +26,23 @@ class Stack1(Generic[T]):
         return len(self.items) == 0
 
 
+class Stack2[T]:
+    def __init__(self) -> None:
+        self.items = []
+
+    def push(self, item: T) -> None:
+        self.items.append(item)
+
+    def pop(self) -> T:
+        if self.is_empty:
+            raise StackIsEmptyException("Stack is empty")
+
+        return self.items.pop()
+
+    @property
+    def is_empty(self) -> bool:
+        return len(self.items) == 0
+
 class Main:
     @staticmethod
     def run() -> None:
