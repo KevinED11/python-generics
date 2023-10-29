b3a3c885c1dc099962b643c6a2a4fe8fba646a81
KevinED11 <mendez_kael@hotmail.com>
2023-10-28 18:53:48 -0700

    add generic stack implementation

--- main.py
+++ main.py
@@ -1,5 +1,29 @@
+from typing import Generic, TypeVar
 
 
+T = TypeVar("T")
+
+
+class StackIsEmptyException(Exception):
+    pass
+
+
+class Stack1(Generic[T]):
+    def __init__(self) -> None:
+        self.items = []
+
+    def push(self, item: T) -> None:
+        self.items.append(item)
+
+    def pop(self) -> T:
+        if self.is_empty:
+            raise StackIsEmptyException("Stack is empty")
+
+        return self.items.pop()
+
+    @property
+    def is_empty(self) -> bool:
+        return len(self.items) == 0
 
 
 class Main:
