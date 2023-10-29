0f5f09a96da85c538469e8667a44eeb6ee9512be
KevinED11 <mendez_kael@hotmail.com>
2023-10-28 19:48:08 -0700

    add generic function with python 3.12

--- generic_functions.py
+++ generic_functions.py
@@ -8,9 +8,16 @@ def get_last_item(items: list[T]) -> T:
     return items[-1]
 
 
+def get_last_item2[T](items: list[T]) -> T:
+    return items[len(items) - 1]
+
+
 def main() -> None:
     elements: list[int] = [1, 2, 3]
     print(get_last_item(elements))
+
+    print(get_last_item2(elements))
+
 
 if __name__ == "__main__":
     main()
