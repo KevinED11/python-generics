85de7e884571e8f00e00d28a95dc8b70e83285a5
KevinED11 <mendez_kael@hotmail.com>
2023-10-28 19:12:08 -0700

    add new features in stacks implementations

--- main.py
+++ main.py
@@ -20,6 +20,15 @@ class Stack1(Generic[T]):
 
         return self.__items.pop()
 
+    def __getitem__(self, index: int) -> T:
+        if 0 > index >= self.__len__():
+            raise IndexError("Index out of range")
+
+        return self.__items[index]
+
+    def __len__(self) -> int:
+        return len(self.__items)
+
     @property
     def is_empty(self) -> bool:
         return len(self.__items) == 0
@@ -38,6 +47,15 @@ class Stack2[T]:
 
         return self.__items.pop()
 
+    def __getitem__(self, index: int) -> T:
+        if 0 > index >= self.__len__():
+            raise IndexError("Index out of range")
+
+        return self.__items[index]
+
+    def __len__(self) -> int:
+        return len(self.__items)
+
     @property
     def is_empty(self) -> bool:
         return len(self.__items) == 0
@@ -46,7 +64,12 @@ class Stack2[T]:
 class Main:
     @staticmethod
     def run() -> None:
-        ...
+        stack: Stack2[int] = Stack2()
+        stack.push(1)
+        stack.push(2)
+        stack.push(3)
+
+        print(stack[-1])
 
 
 if __name__ == "__main__":
